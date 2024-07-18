0e25e3d3ac887652653d36d9f8c256c4f35909f7
suryamsp <murugesansuryamsp@gmail.com>
2024-07-18 06:56:00 -0700

    #

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const PORT = process.env.PORT; 
 const mongo_url = process.env.MONGO_URL;
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://snegamsp.netlify.app'];
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
@@ -107,6 +107,7 @@ async function Addout(data) {
     .collection("olaoutput")
     .insertOne(data);
 }
+
 app.post("/Leave",async function (req, response) {
   const {name,str_date,end_date,reason}= req.body;
   
